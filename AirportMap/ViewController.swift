e2d123e1ae713f58d941407d2c4b8c01624b2c05
joshuacurrie <j3currie@uwaterloo.ca>
2019-05-28 17:47:31 -0400

    Initial Commit

---
+++ AirportMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AirportMap
+//
+//  Created by Joshua Currie on 5/28/19.
+//  Copyright © 2019 Joshua Currie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
